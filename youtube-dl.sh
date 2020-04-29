5f519a39433ae4bd2512e63a53e94450b1d0d88c
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-04-29 16:12:46 -0300

    Program v3

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -7,12 +7,14 @@ echo "Qual formato deseja Salvar: MP3 ou MKV? "
 read mode
 echo "Coloque o Link do Youtube: "
 read link
-echo "[$(tput setaf 14)LINK ] PEGO O LINK DO YOUTUBE [$(link)]"
-echo "[$(tput setaf 13)MODE ] SELECIONADO O MODO PARA DOWNLOAD [$(mode)]"
+echo "[ $(tput setaf 14) LINK $(tput setaf 2) ] PEGO O LINK DO YOUTUBE [$link]"
+echo "[ $(tput setaf 13) MODE $(tput setaf 2) ] SELECIONADO O MODO PARA DOWNLOAD [$mode]"
 if [[ $mode = "MP3" ]]
 then
-    youtube-dl -x --audio-format mp3 $link
+    echo -e youtube-dl -x --audio-format mp3 $link
+fi
 if [[ $mode = "MKV" ]]
 then 
-    youtube-dl $link
-echo "$(tput setaf 1)DONE ] DOWNLOAD DO VIDEO CONCLUIDA!"
+   echo -e youtube-dl $link
+fi
+echo "[ $(tput setaf 3) DONE $(tput setaf 2) ] DOWNLOAD DO VIDEO CONCLUIDA!"
