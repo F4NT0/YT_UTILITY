443f0d79e12d79908044ef0f8288bb9f51d88465
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-04-29 15:35:50 -0300

    program v2

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Como utilizar o programa
 
 1. Abra o programa no Terminal com os comandos <kbd>Ctrl</kbd> + <kbd>Alt</kbd> + <kbd>T</kbd>
-2. Escreva o comando para poder rodar o programa
+2. Vá até o Diretório desejado usando o comando **cd**
+3. Escreva o comando para poder rodar o programa
 	1. **chmod +x youtube-dl.sh**
-3. Para rodar o programa
-	1. **./youtube-dl.sh**
-4. Selecione o modo de download entre **MP3** e **MKV**
-5. Coloque o link do video do youtube e espere terminar
+4. Para rodar o programa
+	2. **./youtube-dl.sh**
+5. Selecione o modo de download entre **MP3** e **MKV**
+6. Coloque o link do video do youtube e espere terminar
--- youtube-dl.sh
+++ youtube-dl.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-printf "------------------------------"
-printf "DOWNLOADS DE VIDEOS DO YOUTUBE"
-printf "------------------------------"
+echo "------------------------------"
+echo "DOWNLOADS DE VIDEOS DO YOUTUBE"
+echo "------------------------------"
 echo ""
 echo "Qual formato deseja Salvar: MP3 ou MKV? "
 read mode
 echo "Coloque o Link do Youtube: "
 read link
-printf "[$(tput setaf 14)LINK ] PEGO O LINK DO YOUTUBE [$(link)]"
-printf "[$(tput setaf 13)MODE ] SELECIONADO O MODO PARA DOWNLOAD [$(mode)]"
+echo "[$(tput setaf 14)LINK ] PEGO O LINK DO YOUTUBE [$(link)]"
+echo "[$(tput setaf 13)MODE ] SELECIONADO O MODO PARA DOWNLOAD [$(mode)]"
 if [[ $mode = "MP3" ]]
 then
     youtube-dl -x --audio-format mp3 $link
 if [[ $mode = "MKV" ]]
 then 
     youtube-dl $link
+echo "$(tput setaf 1)DONE ] DOWNLOAD DO VIDEO CONCLUIDA!"
