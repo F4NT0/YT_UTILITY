7ccd072f3d3accb83447cd30a6c1683d140d8516
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-05-27 20:02:39 -0300

    youtube-dl updated

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -4,30 +4,31 @@
 mp3="youtube-dl -x --audio-format mp3";
 mkv="youtube-dl";
 
-echo "------------------------------"
-echo "DOWNLOADS DE VIDEOS DO YOUTUBE"
-echo "------------------------------"
+echo "-----------------------"
+echo "DOWNLOAD YOUTUBE VIDEOS"
+echo "-----------------------"
 echo ""
-echo "[ $(tput setaf 4) INFO $(tput setaf 2) ] SELECIONE O FORMATO DO VIDEO [MP3 / MKV]: "
+echo "[ $(tput setaf 4) INFO $(tput sgr0) ] SELECT THE FORMAT [MP3 / MKV]: "
 read mode;
-echo "[ $(tput setaf 4) INFO $(tput setaf 2) ] COLE O LINK DO VIDEO AQUI [CTRL + SHIFT + V]: "
+echo "[ $(tput setaf 4) INFO $(tput sgr0) ] PASTE THE URL [CTRL + SHIFT + V]: "
 read link;
-echo "[ $(tput setaf 4) INFO $(tput setaf 2) ] DIGITE O LOCAL DE DESTINO: "
+echo "[ $(tput setaf 4) INFO $(tput sgr0) ] INSERT THE DIRECTORY NAME [Example: Desktop]: "
 read input
 destiny=$(locate -l 1 -b $input) #Localiza e salva o primeiro diretorio encontrado
 cd $destiny
-echo "[ $(tput setaf 13) LINK $(tput setaf 2) ] PEGO O LINK DO YOUTUBE [$link]"
-echo "[ $(tput setaf 13) MODE $(tput setaf 2) ] SELECIONADO O MODO PARA DOWNLOAD [$mode]"
+echo "[ $(tput setaf 13) LINK $(tput sgr0) ] YOUTUBE URL READED [$link]"
+echo "[ $(tput setaf 13) MODE $(tput sgr0) ] DOWNLOAD MODE SELECTED [$mode]"
+echo "[ $(tput setaf 13) DIR  $(tput sgr0) ] DIRECTORY ACCEPTED [$destiny]"
 if [[ $mode = "MP3" ]]
 then
-    echo "[ $(tput setaf 13) RUN $(tput setaf 2) ] FAZENDO DOWNLOAD DO VIDEO"    
+    echo "[ $(tput setaf 13) RUN $(tput sgr0) ] DOWNLOADING...."    
     $mp3 $link
 fi
 if [[ $mode = "MKV" ]]
 then 
-    echo "[ $(tput setaf 3) RUN $(tput setaf 2) ] FAZENDO DOWNLOAD DO VIDEO"
+    echo "[ $(tput setaf 3) RUN $(tput sgr0) ] DOWNLOADING...."
     $mkv $link
 fi
 
 
-echo "[ $(tput setaf 3) DONE $(tput setaf 2) ] DOWNLOAD DO VIDEO CONCLUIDA!"
+echo "[ $(tput setaf 3) DONE $(tput sgr0) ] DOWNLOAD COMPLETE!"
